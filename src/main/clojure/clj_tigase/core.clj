765d4f88dfc73d76fd9af1a6dc7d5effc33b4777
Daniel E. Renfer <duck@kronkltd.net>
2011-04-23 12:31:48 -0400

    new functions

--- src/main/clojure/clj_tigase/core.clj
+++ src/main/clojure/clj_tigase/core.clj
@@ -1,9 +1,20 @@
 (ns clj-tigase.core
   (:use [clojure.string :only (trim)])
   (:import javax.xml.namespace.QName
+           tigase.conf.ConfigurationException
+           tigase.conf.ConfiguratorAbstract
+           tigase.server.Packet
+           tigase.server.XMPPServer
            tigase.xml.Element
-           tigase.server.Packet)
-  )
+           tigase.xmpp.JID))
+
+(def #^:dynamic *message-router* (ref nil))
+
+(def #^:dynamic *configurator-prop-key* "tigase-configurator")
+(defonce #^:dynamic *default-configurator* "tigase.conf.Configurator")
+
+(def #^:dynamic *name* "Tigase")
+(def #^:dynamic *server-name* "message-router")
 
 (declare to-tigase-element)
 
@@ -170,11 +181,6 @@
   (and element
        (= (.getName element) "pubsub")))
 
-(defn packet?
-  "Returns if the element is a packet"
-  [^Element element]
-  (instance? Packet element))
-
 (defn set-packet
   [request body]
   {:body body
@@ -196,3 +202,87 @@
   "given an item element, returns a packet"
   [request ^Element item]
   (.okResult (:packet request) item 0))
+
+(defn make-jid
+  ([user]
+     (make-jid (:username user) (:domain user)))
+  ([user domain]
+     (make-jid user domain ""))
+  ([user domain resource]
+     (JID/jidInstance user domain resource)))
+
+(defn node-value
+  [#^Element element]
+  (.getAttribute element "node"))
+
+(defn make-request
+  [^Packet packet]
+  (let [type (keyword (str (.getType packet)))
+        to (.getStanzaTo packet)
+        from (.getStanzaFrom packet)
+                payload  (first (iq-elements packet))
+        pubsub? (pubsub-element? payload)
+        child-node (first (children payload))
+        node (and child-node (node-value child-node))
+        name (if pubsub?
+               (if child-node (.getName child-node))
+               (if payload (.getName payload)))]
+    {:to to
+     :from from
+     :pubsub pubsub?
+     :payload payload
+     :id (.getAttribute packet "id")
+     :name name
+     :node node
+     :ns (if payload (.getXMLNS payload))
+     :packet packet
+     :request-method type
+     :method type
+     :items (get-items packet)}))
+
+(defn deliver-packet!
+  [^Packet packet]
+  (try
+    (.initVars packet)
+    (.processPacket @*message-router* packet)
+    (catch NullPointerException e
+      #_(error "Router not started: " e)
+      #_(stacktrace/print-stack-trace e)
+      packet)))
+
+
+(defn get-router
+  [args config]
+  (let [mr-class-name (.getMessageRouterClassName config)]
+    (.newInstance (Class/forName mr-class-name))))
+
+(defmacro with-router
+  [router & body]
+  `(binding [jiksnu.xmpp.router/*message-router* ~router]
+     ~@body))
+
+(defn process!
+  [^Packet packet]
+  (.processPacket *message-router* packet))
+
+(defn get-config
+  [initial-config tigase-options]
+  (ConfiguratorAbstract/loadLogManagerConfig initial-config)
+  (let [config-class-name (System/getProperty
+                           *configurator-prop-key*
+                           *default-configurator*)
+        ^ConfiguratorAbstract config
+        (.newInstance (Class/forName config-class-name))]
+    (.init config tigase-options)
+    (.setName config "basic-conf")
+    config))
+
+(defn start-router!
+  [tigase-options config]
+  (dosync
+   (ref-set
+    *message-router*
+    (doto (get-router tigase-options config)
+      (.setName *server-name*)
+      (.setConfig config)
+      .start))))
--- src/test/clojure/clj_tigase/core_test.clj
+++ src/test/clojure/clj_tigase/core_test.clj
@@ -23,17 +23,11 @@
 
 (describe make-element-qname)
 
+(describe get-qname)
+
 (describe assign-namespace)
 
 (describe process-child)
-
-(describe to-tigase-element)
-
-(describe children)
-
-(describe merge-namespaces)
-
-(describe add-attributes)
 
 ;; (describe make-element
 ;;   (testing "with a complex structure"
@@ -46,6 +40,14 @@
 ;;             response  (make-element element-vec)]
 ;;         (expect (element? response))))))
 
+(describe to-tigase-element)
+
+(describe children)
+
+(describe merge-namespaces)
+
+(describe add-attributes)
+
 ;; (describe make-packet
 ;;   (do-it "should return a packet"
 ;;     (let [user (model.user/create (factory User))
@@ -56,3 +58,60 @@
 ;;                              ["pubsub" {}])}
 ;;           response (make-packet packet-map)]
 ;;       (expect (packet? response)))))
+
+(describe iq-elements)
+
+;; TODO: make generic
+(describe pubsub-items
+  (do-it "should return a seq of elements"
+    (let [element (make-element
+                   ["pubsub" {}
+                    ["items" {}
+                     ["item" {} "foo"]
+                     ["item" {} "bar"]]])
+          packet (make-packet
+                  {:to (make-jid "foo" "example.com")
+                   :from (make-jid "bar" "example.com")
+                   :type :set
+                   :body element})
+          response (pubsub-items packet)]
+      (expect (every? element? response)))))
+
+(describe bare-recipient?)
+
+(describe get-items)
+
+(describe pubsub-element?)
+
+(describe set-packet)
+
+(describe result-packet)
+
+(describe respond-with)
+
+(describe make-jid)
+
+(describe node-value)
+
+(describe make-request
+  (testing "a pubsub publish"
+    (do-it "should return a map"
+      (let [packet (make-packet
+                    {:to (make-jid "foo" "example.com")
+                     :from (make-jid "bar" "example.com")
+                     :type :get})
+            response (make-request packet)]
+        (expect (map? response))))))
+
+
+(describe deliver-packet!)
+
+(describe get-router)
+
+(describe with-router)
+
+(describe process!)
+
+(describe get-config)
+
+(describe start-router!)
