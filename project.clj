8773a0320d449c06130d58253a76ea1a3946de6d
Daniel E. Renfer <duck@kronkltd.net>
2012-09-09 10:50:38 -0400

    bump dependencies and move to lein 2

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject clj-tigase "0.1.0-SNAPSHOT"
   :description "Clojure library for working with Tigase XMPP Server"
   :url "http://github.com/duck1123/clj-tigase"
+  :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"tigase-snapshots" "http://maven.tigase.org/"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [tigase/tigase-server "5.1.0-SNAPSHOT"]]
-  :dev-dependencies [[midje "1.4.0"]
-                     [lein-midje "1.0.10"]]
-  :warn-on-reflection true
-  :jvm-opts ["-server"])
+  :profiles {:dev
+             {:dependencies
+              [[midje "1.5.0-SNAPSHOT"]]}}
+  :plugins [[codox "0.6.1"]
+            [lein-midje "2.0.0-SNAPSHOT"]])
