62130c64e36d29a0c5701b143d9b76d998af671b
Daniel E. Renfer <duck@kronkltd.net>
2015-03-15 09:11:05 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"tigase-snapshots" "http://maven.tigase.org/"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [tigase/tigase-server "5.1.3"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [tigase/tigase-server "7.0.0"]]
   :profiles {:dev
              {:dependencies
-              [[midje "1.5-alpha3"]]}}
-  :plugins [[codox "0.6.4"]
-            [lein-midje "2.0.3"]])
+              [[midje "1.7.0-SNAPSHOT"]]}}
+  :plugins [[codox "0.8.10"]
+            [lein-midje "3.1.3"]])
